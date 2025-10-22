4ded85c2d7284362ad023806280f9a4a5239f26d
PONLOK-POV <ponlok-pov@mptc.gov.kh>
2025-10-22 14:27:25 +0700

    fix: delete icon

--- app/components/education.tsx
+++ app/components/education.tsx
@@ -1,15 +1,7 @@
 import React from 'react'
 import Text from "../components/ui/text";
-import { FaFacebookF, FaInstagram, FaTelegramPlane } from "react-icons/fa";
-import { FiMail } from 'react-icons/fi';
 
 const Education = () => {
-    const socialIcons = [
-        { icon: FaFacebookF, key: "facebook", url: "https://www.facebook.com/share/1A3fkbv2h4/" },
-        { icon: FaTelegramPlane, key: "telegram", url: "https://t.me/+85517465395" },
-        { icon: FaInstagram, key: "instagram", url: "https://www.instagram.com/pov.ponlok.336/profilecard/?igsh=MXRycnY4N290Y3Q4Yw==" },
-        { icon: FiMail, key: "mail", url: "https://ponlokpov2@gmail.com" },
-      ];
   return (
    <section className='container grid gap-4 py-8'>
     <Text variant="subheading" className="text-center text-blue-400"> 
